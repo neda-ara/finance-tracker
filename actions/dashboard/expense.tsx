833c6120f82b51d5dafe8cab768182b1ef03396b
Neda Ara <neda.ara9@gmail.com>
2026-02-01 21:52:54 +0530

    Add server actions and hooks for update and delete expense

--- actions/dashboard/expense.tsx
+++ actions/dashboard/expense.tsx
@@ -113,3 +113,95 @@ export async function createExpense(
     );
   });
 }
+
+export async function updateExpense(
+  formData: FormData
+): Promise<ActionResult<void>> {
+  return safeRunAction(async () => {
+    const session = await getAuthenticatedSession();
+
+    const expenseId = formData.get("id");
+
+    if (!expenseId) {
+      throw { message: "Expense ID is missing" };
+    }
+
+    const input = {
+      amount: Number(formData.get("amount")),
+      currency: formData.get("currency"),
+      category: formData.get("category"),
+      paymentMode: formData.get("paymentMode"),
+      description: formData.get("description"),
+      satisfactionRating: Number(formData.get("satisfactionRating")),
+      expenseDate: formData.get("expenseDate"),
+    };
+
+    const parsedInput = expenseInputSchema.safeParse(input);
+
+    if (!parsedInput.success) {
+      throw {
+        fieldErrors: Object.fromEntries(
+          Object.entries(parsedInput.error.flatten().fieldErrors)
+            .filter(([, v]) => v?.length)
+            .map(([k, v]) => [k, v![0]])
+        ),
+      };
+    }
+
+    const values = parsedInput.data;
+
+    const result = await db.query(
+      `
+    UPDATE expenses
+    SET
+      amount = $1,
+      expense_date = $2,
+      category = $3,
+      currency = $4,
+      payment_mode = $5,
+      description = $6,
+      satisfaction_rating = $7
+    WHERE id = $8 AND user_id = $9
+    `,
+      [
+        values.amount,
+        values.expenseDate,
+        values.category,
+        values.currency,
+        values.paymentMode,
+        values.description,
+        values.satisfactionRating,
+        expenseId,
+        session?.userId,
+      ]
+    );
+
+    if (result.rowCount === 0) {
+      throw { message: "Requested expense not found" };
+    }
+  });
+}
+
+export async function deleteExpense(
+  expenseId: string
+): Promise<ActionResult<void>> {
+  return safeRunAction(async () => {
+    const session = await getAuthenticatedSession();
+
+    if (!expenseId) {
+      throw { message: "Expense ID is missing" };
+    }
+
+    const result = await db.query(
+      `
+      DELETE FROM expenses
+      WHERE id = $1 AND user_id = $2
+      `,
+      [expenseId, session?.userId]
+    );
+
+    if (result.rowCount === 0) {
+      throw { message: "Requested expense not found" };
+    }
+  });
+}
--- hooks/use-expenses.ts
+++ hooks/use-expenses.ts
@@ -1,6 +1,11 @@
 "use client";
 
-import { createExpense, fetchExpenses } from "@/actions/dashboard/expense";
+import {
+  createExpense,
+  deleteExpense,
+  fetchExpenses,
+  updateExpense,
+} from "@/actions/dashboard/expense";
 import { EXPENSE_CLIENT_QUERY_KEY } from "@/lib/constants/query-keys";
 import { GetExpensesRequest } from "@/lib/actions/types";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
@@ -33,6 +38,18 @@ export function useExpenses(params: GetExpensesRequest) {
         queryClient.invalidateQueries({
           queryKey: [EXPENSE_CLIENT_QUERY_KEY],
         });
+      },
+    }),
+    update: useMutation({
+      mutationFn: updateExpense,
+      onSuccess: () => {
+        queryClient.invalidateQueries({ queryKey: [EXPENSE_CLIENT_QUERY_KEY] });
+      },
+    }),
+    delete: useMutation({
+      mutationFn: deleteExpense,
+      onSuccess: () => {
+        queryClient.invalidateQueries({ queryKey: [EXPENSE_CLIENT_QUERY_KEY] });
       },
     }),
   };
