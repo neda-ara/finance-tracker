b242020d7df8b313a96cf70fa4b81f125c8f8b24
Neda Ara <neda.ara9@gmail.com>
2026-02-23 23:54:41 +0530

    Add sidebar initial layout

--- components/common/sidebar.tsx
+++ components/common/sidebar.tsx
@@ -1,5 +1,11 @@
 "use client";
 
 export const Sidebar = () => {
-  return <div className="bg-transparent w-60"></div>;
+  return (
+    <div className="bg-transparent w-60 h-full">
+      <div className="">Logo</div>
+      <div>MENU ITEMS</div>
+      <div>Profile & Logout</div>
+    </div>
+  );
 };
