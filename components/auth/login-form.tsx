47817f8afb875fb435a674aa2255c6a5ba175bcb
Neda Ara <neda.ara9@gmail.com>
2026-02-25 23:52:06 +0530

    Add loader to login form

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -15,6 +15,7 @@ import { Input } from "../ui/input";
 import { loginInputSchema } from "@/lib/schema/login-schema";
 import { ROUTES } from "@/lib/constants/constants";
 import { SocialMediaOptions } from "./social-media-options";
+import { Spinner } from "../ui/spinner";
 import { useAuth } from "@/hooks/use-auth";
 import { useForm } from "react-hook-form";
 import { useRouter } from "next/navigation";
@@ -79,6 +80,8 @@ export const LoginForm = () => {
     input:
       "pl-12 focus-visible:border-2 focus-visible:border-(--color-cta) focus-visible:ring-0 selection:bg-(--color-primary) selection:text-black",
   };
+
+  const loginInProgress = login.isPending;
 
   return (
     <div className="flex flex-col w-full space-y-4 px-6">
@@ -158,8 +161,13 @@ export const LoginForm = () => {
           >
             Forgot Password?
           </Button>
-          <Button type="submit" variant="cta" className="w-full my-2">
-            Log In <MoveRight />
+          <Button
+            type="submit"
+            variant="cta"
+            className="w-full my-2"
+            disabled={loginInProgress}
+          >
+            Log In {loginInProgress ? <Spinner /> : <MoveRight />}
           </Button>
         </form>
       </Form>
