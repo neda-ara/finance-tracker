b10f394ea4a7a827830d1f568bc32093fa02b710
Neda Ara <neda.ara9@gmail.com>
2026-02-26 00:11:21 +0530

    Enhance logo

--- components/common/logo.tsx
+++ components/common/logo.tsx
@@ -1,28 +1,42 @@
 "use client";
 
-import { ChartNoAxesCombined } from "lucide-react";
-import { Coiny } from "next/font/google";
+import { ChartNoAxesCombined, DollarSign } from "lucide-react";
+import { cn } from "@/lib/utils/shadcn-utils";
+import { Coiny, Baloo_Tamma_2 } from "next/font/google";
 
 const coiny = Coiny({
   weight: ["400"],
 });
 
+const bt2 = Baloo_Tamma_2({
+  weight: ["400", "500", "600", "700", "800"],
+});
+
 export const Logo = () => {
   return (
     <div className="flex items-center gap-2 px-4 py-6">
-      <div className="bg-white/20 backdrop-blur-sm p-2 rounded-lg">
+      <div className="relative bg-white/20 backdrop-blur-sm p-2 rounded-lg">
         <ChartNoAxesCombined className="text-slate-900 w-7 h-7" />
+        <DollarSign className="absolute left-4 top-1.25 -rotate-45 h-3 w-3" />
       </div>
 
       <div className="flex flex-col leading-tight">
         <h1
-          className={`${coiny.className} text-3xl font-semibold tracking-wide leading-5 mt-1.5`}
+          className={cn(
+            "text-3xl font-semibold tracking-wide leading-5 mt-2",
+            coiny.className
+          )}
         >
           <span className="text-slate-900">Clari</span>
           <span className="text-white">Fi</span>
         </h1>
 
-        <p className="text-xs font-medium text-slate-900/80 leading-3">
+        <p
+          className={cn(
+            "text-sm font-medium text-slate-900/80 leading-4",
+            bt2.className
+          )}
+        >
           Bring clarity to finance
         </p>
       </div>
--- components/common/sidebar.tsx
+++ components/common/sidebar.tsx
@@ -1,9 +1,11 @@
 "use client";
 
+import { Logo } from "./logo";
+
 export const Sidebar = () => {
   return (
-    <div className="bg-transparent w-60 h-full">
-      <div className="">Logo</div>
+    <div className="bg- w-60 h-full">
+      <Logo />
       <div>MENU ITEMS</div>
       <div>Profile & Logout</div>
     </div>
