a5d7e8f7f2714ef55e77a3619925e107c0525810
Neda Ara <neda.ara9@gmail.com>
2026-04-07 00:22:43 +0530

    Fix max height issue for legend div

--- components/overview/category-pie-chart.tsx
+++ components/overview/category-pie-chart.tsx
@@ -15,7 +15,7 @@ export const CategoryPieChart = ({
   data: DataType[] | undefined;
 }) => {
   return (
-    <div className="w-full h-full flex items-center gap-x-2">
+    <div className="max-h-full w-full h-full flex items-center gap-x-2 pt-3">
       <ResponsiveContainer width="100%" height="100%">
         <PieChart>
           <Pie
@@ -38,26 +38,21 @@ export const CategoryPieChart = ({
           />
         </PieChart>
       </ResponsiveContainer>
-      <div className="max-h-full h-full min-h-0 w-1/2 overflow-y-scroll custom-scrollbar pr-2 mr-4">
-        <div className="max-h-full min-h-0 flex flex-col gap-2 text-xs">
-          {data?.map((item, index) => (
-            <div
-              key={index}
-              className="flex items-center justify-between gap-2"
-            >
-              <div className="flex items-center gap-2 min-w-0">
-                <div
-                  className="w-2.5 h-2.5 rounded-full shrink-0"
-                  style={{ backgroundColor: item.fill }}
-                />
-                <span className="truncate text-gray-700">{item.name}</span>
-              </div>
-              <span className="text-gray-900 font-medium whitespace-nowrap">
-                ₹{item.value.toLocaleString("en-IN")}
-              </span>
+      <div className="max-h-[calc(100vh-575px)] h-full min-h-0 w-2/3 overflow-y-scroll custom-scrollbar pr-3 mr-1 flex flex-col gap-2 text-xs">
+        {data?.map((item, index) => (
+          <div key={index} className="flex items-center justify-between gap-2">
+            <div className="flex items-center gap-2 min-w-0">
+              <div
+                className="w-2.5 h-2.5 rounded-full shrink-0"
+                style={{ backgroundColor: item.fill }}
+              />
+              <span className="truncate text-gray-700">{item.name}</span>
             </div>
-          ))}
-        </div>
+            <span className="text-gray-900 font-medium whitespace-nowrap">
+              ₹{item.value.toLocaleString("en-IN")}
+            </span>
+          </div>
+        ))}
       </div>
     </div>
   );
