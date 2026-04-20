41b08c4a830c19c658339d4235e5c821e6296f73
Neda Ara <neda.ara9@gmail.com>
2026-04-20 23:51:32 +0530

    Add Generate AI Insights button to OVerview page

--- app/(dashboard)/overview/page.tsx
+++ app/(dashboard)/overview/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import { Overview } from "@/components/overview/overview";
 import { PageTitle } from "@/components/common/page-title";
 import type { Metadata } from "next";
@@ -10,10 +11,13 @@ export const metadata: Metadata = {
 export default function OverviewPage() {
   return (
     <div className="relative flex flex-col py-4 px-6 gap-y-6 min-h-[calc(100vh-16px)] max-h-[calc(100vh-16px)] overflow-hidden">
-      <PageTitle
-        heading="Overview"
-        subheading="Get a snapshot of your finances in one glance."
-      />
+      <div className="flex items-center justify-between">
+        <PageTitle
+          heading="Overview"
+          subheading="Get a snapshot of your finances in one glance."
+        />
+        <Button>Generate AI Insights</Button>
+      </div>
       <Overview />
     </div>
   );
